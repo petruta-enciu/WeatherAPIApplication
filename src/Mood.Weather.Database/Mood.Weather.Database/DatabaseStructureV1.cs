872ee79765e86d3caf8ac22ed77f2ad8793a1197
petruta-enciu <p.enciu@gmail.com>
2013-06-11 23:31:23 +0300

    Database live Migrator project

---
+++ src/Mood.Weather.Database/Mood.Weather.Database/DatabaseStructureV1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator;
+
+namespace Mood.Weather.Database
+{
+    [Migration(1)]
+    public class DatabaseStructureV1 : Migration
+    {
+        public override void Up()
+        {
+            Execute.Script("weather_v1.sql");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
